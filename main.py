e6a95aeb9412f8424475c50c163388334ff2c51e
jakeaster <jakeaster@pm.me>
2023-08-24 20:35:10 -0700

    commit

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
+    original_str = "Python Programming"
+    sub1 = original_str[0:6]
+    sub2 = original_str[7:18]
+    merged_str = f'{sub2} {sub1}'
+
+    print(sub2)
+    print(sub1)
+    print(merged_str)
 
 
 if __name__ == '__main__':
